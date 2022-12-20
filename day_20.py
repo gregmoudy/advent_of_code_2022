f169b915a112256ffe6200a6c696c5d01b0e0e62
Greg Moudy <gregmoudy@gmail.com>
2022-12-20 14:29:06 -0600

    Day 20 - Part 1 and 2 Solved

--- day_20.py
+++ day_20.py
@@ -49,14 +49,70 @@
 
 # 4 moves between -3 and 0:
 # 1, 2, -3, 4, 0, 3, -2
-# Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3000th numbers after the value 0, wrapping around the list as necessary. In the above example, the 1000th number after 0 is 4, the 2000th is -3, and the 3000th is 2; adding these together produces 3.
+# Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3000th numbers after the value 0, wrapping around the list as necessary. 
+# In the above example, the 1000th number after 0 is 4, the 2000th is -3, and the 3000th is 2; adding these together produces 3.
 
 # Mix your encrypted file exactly once. What is the sum of the three numbers that form the grove coordinates?
+
+# --- Part Two ---
+# The grove coordinate values seem nonsensical. While you ponder the mysteries of Elf encryption, 
+# you suddenly remember the rest of the decryption routine you overheard back at camp.
+
+# First, you need to apply the decryption key, 811589153. Multiply each number by the decryption key before you begin; 
+# this will produce the actual list of numbers to mix.
+
+# Second, you need to mix the list of numbers ten times. The order in which the numbers are mixed does not change during mixing; 
+# the numbers are still moved in the order they appeared in the original, pre-mixed list. 
+# (So, if -3 appears fourth in the original list of numbers to mix, 
+# -3 will be the fourth number to move during each round of mixing.)
+
+# Using the same example as above:
+
+# Initial arrangement:
+# 811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612
+
+# After 1 round of mixing:
+# 0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153
+
+# After 2 rounds of mixing:
+# 0, 2434767459, 1623178306, 3246356612, -2434767459, -1623178306, 811589153
+
+# After 3 rounds of mixing:
+# 0, 811589153, 2434767459, 3246356612, 1623178306, -1623178306, -2434767459
+
+# After 4 rounds of mixing:
+# 0, 1623178306, -2434767459, 811589153, 2434767459, 3246356612, -1623178306
+
+# After 5 rounds of mixing:
+# 0, 811589153, -1623178306, 1623178306, -2434767459, 3246356612, 2434767459
+
+# After 6 rounds of mixing:
+# 0, 811589153, -1623178306, 3246356612, -2434767459, 1623178306, 2434767459
+
+# After 7 rounds of mixing:
+# 0, -2434767459, 2434767459, 1623178306, -1623178306, 811589153, 3246356612
+
+# After 8 rounds of mixing:
+# 0, 1623178306, 3246356612, 811589153, -2434767459, 2434767459, -1623178306
+
+# After 9 rounds of mixing:
+# 0, 811589153, 1623178306, -2434767459, 3246356612, 2434767459, -1623178306
+
+# After 10 rounds of mixing:
+# 0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153
+# The grove coordinates can still be found in the same way. Here, the 1000th number after 0 is 811589153, 
+# the 2000th is 2434767459, and the 3000th is -1623178306; adding these together produces 1623178306.
+
+# Apply the decryption key and mix your encrypted file ten times. What is the sum of the three numbers that form the grove coordinates?
+
 
 
 import timeit
 import tqdm
     
+
+DECRYPTION_KEY = 811589153
+
 
 
 def read_input(file_path):
@@ -77,67 +133,38 @@ class Number_Tracker:
         return f'Start Index: {self.start_index} Number: {self.num}'
 
 
-def process_data(numbers):
+def process_data(numbers, use_decryption_key = False, max_number_of_mixes = 1):
     item_0 = None
     original_order = list()
+    mix_count = 0
+
     for i, n in enumerate(numbers):
+        if use_decryption_key:
+            n *= DECRYPTION_KEY
+
         number_tracker = Number_Tracker(n,i)
         original_order.append(number_tracker)
 
     mixed_list = list(original_order)
-    for item in tqdm.tqdm(original_order):
-        if item.num == 0:
-            item_0 = item
-            continue
 
-        current_index = mixed_list.index(item)
-        new_index = current_index + item.num
-        new_index_flat = (new_index % len(mixed_list))
+    while mix_count < max_number_of_mixes:
+        mix_count += 1
+        for item in tqdm.tqdm(original_order):
+            if item.num == 0:
+                item_0 = item
+                continue
 
-        # if item.num == 4:
-        #     print('break')
-        
-        # if item.num < 0:
-        #     if new_index_flat == 0:
-        #         new_index_flat = len(mixed_list)
-        #     else:
-        #         new_index_flat -= 1
+            current_index = mixed_list.index(item)
+            new_index = current_index + item.num
+            new_index_wrapped = (new_index % (len(mixed_list)-1))
+            
+            if new_index_wrapped == current_index:
+                continue
 
-        # if item.num > 0:
-        #     if new_index != new_index_flat:
-        #         new_index_flat += 1
+            mixed_list.remove(item)
+            mixed_list.insert(new_index_wrapped, item)
 
-        #if new_index == 0:
-            #new_index == len(mixed_list)
-
-        # if new_index >= len(mixed_list):
-        #     new_index = (new_index % len(mixed_list)) + 1
-        
-        # elif new_index < 0:
-        #     new_index = (new_index % len(mixed_list)) - 1
-        
-        if new_index_flat == current_index:
-            continue
-
-        mixed_list.remove(item)
-        mixed_list.insert(current_index, None)
-
-        # if new_index == 0:
-        #     mixed_list.append(item)
-        
-        # else:
-        
-        if item.num > 0:
-            x = 1
-        else:
-            x = 0
-            if new_index_flat == 0:
-                new_index_flat = len(mixed_list)
-
-        mixed_list.insert(new_index_flat + x, item)
-        mixed_list.remove(None)
-
-    item_0_index = mixed_list.index(item_0) # 4
+    item_0_index = mixed_list.index(item_0)
     item_0_index_plus_1000 = (item_0_index + 1000) % len(mixed_list)
     item_0_index_plus_2000 = (item_0_index + 2000) % len(mixed_list)
     item_0_index_plus_3000 = (item_0_index + 3000) % len(mixed_list)
@@ -162,10 +189,10 @@ def run():
     print(f'Mix your encrypted file exactly once. What is the sum of the three numbers that form the grove coordinates? : {answer_1}') # 8764 How????
 
     # Part 2 Answer
-    #answer_2_sample = process_data(input_data_sample, settled_rock_count_max = 1000000000000)
-    #print(f'Answer 2 Sample: {answer_2_sample}') # 1514285714288
-    #answer_2 = process_data(input_data, settled_rock_count_max = 2022)
-    #print(f'How tall will the tower be after 1000000000000 rocks have stopped? : {answer_2}') # 
+    answer_2_sample = process_data(input_data_sample, use_decryption_key = True, max_number_of_mixes = 10)
+    print(f'Answer 2 Sample: {answer_2_sample}') # 1623178306
+    answer_2 = process_data(input_data, use_decryption_key = True, max_number_of_mixes = 10)
+    print(f'Apply the decryption key and mix your encrypted file ten times. What is the sum of the three numbers that form the grove coordinates? : {answer_2}') # 535648840980
 
 
 
